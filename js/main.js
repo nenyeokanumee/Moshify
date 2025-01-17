adda3932a562b06a23f7949b73c0c4a29c8db0c5
Nenye Okanumee <nenye.okanumee@gmail.com>
2025-01-17 21:22:46 +0100

    Put it all together

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+// Set up collapsibles
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+ // Get the current year
+ const currentYear = new Date().getFullYear();
+
+ // Set the current year to the span element
+ document.getElementById('current-year').textContent = currentYear;
